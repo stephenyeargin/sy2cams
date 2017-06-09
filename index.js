3f265843cb7f7410aae00e3cef93e65e887059f4
Stephen Yeargin <stephen.yeargin@gmail.com>
2017-06-09 13:13:16 -0500

    Less noise

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-'use strict';
-
 process.on('uncaughtException', function(err) {
   console.log('Caught exception: ' + err);
   console.log(err.stack);
 });
 
-const port = process.env.PORT || 80;
-const express = require('express');
-const raspividStream = require('raspivid-stream');
+// Configure exposed port
+var port = process.env.PORT || 80;
 
-const app = express();
-const wss = require('express-ws')(app);
+// Configure express
+var express = require('express');
+var app = express();
+var wss = require('express-ws')(app);
 
-// Configuring rendering iamge
+// Configure video streaming
+var raspividStream = require('raspivid-stream');
+
+// Configure rendering engine
 app.set('view engine', 'pug');
 
 // Home route
@@ -21,30 +23,42 @@ app.get('/', function (req, res) {
 });
 
 // Websocket video
-app.ws('/video-stream', (ws, req) => {
+app.ws('/video-stream', function (ws, req) {
     console.log('Client connected');
 
     ws.send(JSON.stringify({
       action: 'init',
       width: '960',
       height: '540'
-    })), (error) => { if (error) console.error(error); };
+    }), function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
 
     var videoStream = raspividStream({ rotation: 180 });
 
-    videoStream.on('data', (data) => {
-        ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+    videoStream.on('data', function (data) {
+        ws.send(data, { binary: true }, function (error) {
+          if (error) {
+            console.log(error);
+          }
+        });
     });
 
-    ws.on('close', () => {
+    ws.on('close', function () {
         console.log('Client left');
         videoStream.removeAllListeners('data');
     });
 });
 
+// Handle exceptions
 app.use(function (err, req, res, next) {
   console.error(err);
   next(err);
-})
+});
 
-app.listen(port, () => console.log('Server started on ' + port));
+// Launch server
+app.listen(port, function () {
+  console.log('Server started on ' + port);
+});
